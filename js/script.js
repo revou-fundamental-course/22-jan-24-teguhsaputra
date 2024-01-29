fec32f54eca71ceae0a2069076fe17d14ccc65ad
Teguh Saputra <teguhsaputra01@gmail.com>
2024-01-30 00:05:07 +0700

    validation contact form

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 // slide show
 
 let slideIndex = 1;
+let notif = document.getElementById("notif");
+let error = document.getElementById("error");
+
 showSlides(slideIndex);
 setInterval(autoMoveSlides, 5000);
+error.style.display = "none";
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -30,6 +34,55 @@ function showSlides(n) {
 
 function autoMoveSlides() {
     plusSlides(1);
+}
+
+function sendButtonContact() {
+    let fullname = document.getElementById("fullname").value;
+    let email = document.getElementById("email").value;
+    let interest = document.getElementById("interest").value;
+
+
+    if (notif.innerText === "") {
+        error.style.display = "none";
+    }
+
+
+    if (fullname === "" || email === "" || interest === "") {
+        error.style.display = "";
+        notif.innerText = "All fields must be filled!";
+        return;
+    }
+
+    if (!/^[a-zA-Z ]+$/.test(fullname)) {
+        error.style.display = "";
+        notif.innerText = "Fullname must be letters only!";
+        return;
+    }
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        error.style.display = "";
+        notif.innerText = "Email must be valid!";
+        return;
+    }
+
+    if (interest === "") {
+        error.style.display = "";
+        notif.innerText = "Interest must be selected!";
+        return;
+    }
+
+    clearInput();
+    error.style.display = "none";
+    notif.innerText = "";
+    alert("Thank you for your interest!");
+
+}
+
+function clearInput() {
+    document.getElementById("fullname").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("interest").selectedIndex = 0
 }
 
 // navbar
